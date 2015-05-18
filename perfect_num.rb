277d23117464ae49f267673e95315188b00e042f
Fukuo Kadota <app.fk91@gmail.com>
2015-05-18 21:01:21 +0900

    Perfect number

--- perfect_num.rb
+++ perfect_num.rb
@@ -1,14 +1,18 @@
 number = gets.to_i
 
-sum = 0
-for i in 1..number-1
-  if n % i == 0
-    sum += i
+def perfect_num(number)
+  sum = 0
+  for i in 1..number-1
+    if n % i == 0
+      sum += i
+    end
+  end
+
+  if number == sum
+    puts "#{number} is perfect number"
+  else
+    puts "#{number} is not perfect number"
   end
 end
 
-if number == sum
-  puts "#{number} is perfect number"
-else
-  puts "#{number} is not perfect number"
-end
+perfect_num(number)
