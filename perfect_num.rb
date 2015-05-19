461bf975800acd2149560b64906957938e27f074
Fukuo Kadota <app.fk91@gmail.com>
2015-05-19 23:53:35 +0900

    add method to integer

--- perfect_num.rb
+++ perfect_num.rb
@@ -1,18 +1,17 @@
 number = gets.to_i
 
-def perfect_num(number)
-  sum = 0
-  for i in 1..number-1
-    if n % i == 0
-      sum += i
+class Integer
+  def perfect?
+    number = self
+    sum = 0
+    for i in 1..number-1
+      if number % i == 0
+        sum += i
+      end
     end
-  end
 
-  if number == sum
-    puts "#{number} is perfect number"
-  else
-    puts "#{number} is not perfect number"
+    number == sum
   end
 end
 
-perfect_num(number)
+puts number.perfect?
