7a5a6e380cd866cbbf2a39ab6eb58b949d8f18b1
Fukuo Kadota <app.fk91@gmail.com>
2015-05-20 23:49:54 +0900

    it's perfect answer

--- perfect_num.rb
+++ perfect_num.rb
@@ -1,17 +1,18 @@
-number = gets.to_i
+n = gets.to_i
 
 class Integer
   def perfect?
     number = self
-    sum = 0
-    for i in 1..number-1
-      if number % i == 0
-        sum += i
-      end
-    end
 
-    number == sum
+    # sum = 0
+    # for i in 1..number-1
+    #   if number % i == 0
+    #     sum += i
+    #   end
+    # end
+
+    number == (1..number).select{|i| number % i == 0}.inject(:+)
   end
 end
 
-puts number.perfect?
+puts n.perfect?
