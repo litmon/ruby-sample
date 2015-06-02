0197d52a1260aaf0008a495efd7fac9f97fe907f
Fukuo Kadota <app.fk91@gmail.com>
2015-06-02 16:26:55 +0900

    Program exit when input ENTER

--- scraping.rb
+++ scraping.rb
@@ -1,26 +1,28 @@
-require 'open-uri'
-require 'nokogiri'
+require "open-uri"
+require "nokogiri"
 
-puts 'Input URL you want to scraping and Enter.'
-print '> '
+puts "Input URL you want to scraping and Enter."
+print "> "
 url = STDIN.gets
 
-puts 'Site connecting....'
+puts "Site connecting...."
 
 html = open(url).read
 doc = Nokogiri::HTML(html, url)
 
 unless doc
-  puts 'Sorry! we could not get the site.'
-  return
+  puts "Sorry! we could not get the site. #{url}"
+  exit
 end
 
-puts 'Get page.'
+puts "Get page."
 
 while true
-  puts 'Please input css selector.'
+  puts "Please input css selector. (please ENTER to finish)"
   print "> "
-  selector = STDIN.gets.chomp()
+  selector = STDIN.gets.chomp
+
+  break if selector == ""
 
   puts "---Node Information---"
   puts "Node: #{doc.css(selector)}"
@@ -28,3 +30,4 @@ while true
   puts "--- ---"
 end
 
+puts "See you again!"
